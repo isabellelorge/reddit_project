338a36f250cffa20506309c2ca7d9e5533aac3c0
Isabelle Lorge <isabelle.lorge@outlook.com>
2022-07-05 10:47:50 +0200

    documenting

---
+++ analyses/bert_predictions.py
@@ -0,0 +1,75 @@
+import numpy as np
+from transformers import BertTokenizer, TFBertForMaskedLM
+import tensorflow as tf
+
+# tokenizer = BertTokenizer.from_pretrained('bert-large-cased')
+# model = TFBertForMaskedLM.from_pretrained('bert-large-cased')
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+model = TFBertForMaskedLM.from_pretrained('bert-base-cased')
+
+def get_top_k_predictions(input_string, target, k=10, tokenizer=tokenizer, model=model):
+    '''
+    Function to get bert ranking and pred for not, for target and top k predictions
+    '''
+
+    tokenized_inputs = tokenizer(input_string, return_tensors="tf")
+    outputs = model(tokenized_inputs["input_ids"], return_dict = True)
+    mask_token = tokenizer.encode('[MASK]')[-2]
+
+    # get the index of adv in voc
+    adv_token = tokenizer.encode([target])[1] 
+    neg_token = tokenizer.encode(['not'])[1] 
+
+    # get the index of adv in sentence
+    adv_index = np.where(tokenized_inputs['input_ids'].numpy()[0] == mask_token)[0][0]
+
+    # get the top k logits for adv position in sentence
+    top_k_indices = tf.math.top_k(outputs.logits, k).indices[0].numpy()[adv_index]
+
+    # get softmax over all voc for adv position in sentence
+    all_prob = tf.nn.softmax(outputs.logits[0][adv_index]).numpy()
+    sorted_prob = tf.argsort(all_prob, direction='DESCENDING').numpy()
+
+    # get top prob
+    top_k_prob = all_prob [top_k_indices]
+
+    # get words for top prob
+    decoded_top = tokenizer.decode(top_k_indices)
+
+    # get prob for adv 
+    adv_prob =  all_prob[adv_token]
+    adv_rank = np.where(sorted_prob == adv_token)[0][0]
+
+    # get prob for not 
+    neg_prob = all_prob[neg_token]
+    neg_rank = np.where(sorted_prob == neg_token)[0][0]
+    
+    return (neg_rank, neg_prob), (adv_rank, adv_prob), list(zip(decoded_top.split(), top_k_prob))
+
+
+
+def get_negation_performance(df, predictions_column):
+    '''
+    Get number of top 1 and top 10 'not' predictions,
+    as well as number of times 'not' ranked above target
+    '''
+    top_1 = 0
+    top_10 = 0
+    above_original = 0
+
+    for i, p in df_sent[predictions_column].iteritems():
+        adv = df_sent['sentences'].iloc[i].split()[-2]
+        if p:
+            print(p[1])
+            print(p[2])
+            if p[2][0][0] == 'not':
+                top_1 +=1
+            if any(i[0] == 'not' for i in p[2]):
+                top_10 +=1
+            if adv.strip() not in ['never', 'hardly']:
+                adv_idx = p[1][0]
+                not_idx = p[0][0]
+                if not_idx < adv_idx: # smaller idx means not is ranked above
+                    above_original +=1
+    
+    return top_1, top_10, above_original
